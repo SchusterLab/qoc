work on getting correct gradients

qoc/core/gsd.py
@@ -13,12 +13,14 @@ import scipy.linalg as la
 
 from qoc.models import (MagnusPolicy, OperationPolicy, GrapeSchroedingerDiscreteState,
                         GrapeSchroedingerPolicy, GrapeResult,
-                        InterpolationPolicy, Reporter)
+                        InterpolationPolicy, Dummy)
 from qoc.standard import (Adam, ForbidStates, TargetInfidelity, expm)
-from qoc.util import (PAULI_X, PAULI_Y, conjugate_transpose,
+from qoc.util import (PAULI_X, PAULI_Y, PAULI_Z, conjugate_transpose,
                       matrix_to_column_vector_list, matmuls, ans_jacobian,
                       complex_to_real_imag_flat,
-                      real_imag_to_complex_flat)
+                      real_imag_to_complex_flat,
+                      get_annihilation_operator,
+                      get_creation_operator)
 
 
 ### MAIN METHODS ###
@@ -115,13 +117,12 @@ def grape_schroedinger_discrete(costs, hamiltonian, initial_states,
     
     # Switch on the GRAPE implementation method.
     if gstate.grape_schroedinger_policy == GrapeSchroedingerPolicy.TIME_EFFICIENT:
-        _grape_schroedinger_discrete_time(gstate, initial_params)
+        result = _grape_schroedinger_discrete_time(gstate, initial_params)
     else:
         pass
         # _grape_schroedinger_discrete_memory(gstate, initial_params)
 
-    # return GrapeResult(reporter.error, reporter.grads,
-    #                    reporter.params, reporter.states)
+    return result
 
 
 ### HELPER METHODS ###
@@ -134,7 +135,9 @@ def _grape_schroedinger_discrete_time(gstate, initial_params):
     gstate :: qoc.GrapeSchroedingerDiscreteState - information required to
          perform the optimization
     initial_params :: numpy.ndarray - the transformed initial_params
-    Returns: none
+    Returns: 
+    result :: qoc.GrapeResult - an object that tracks important information
+        about the optimization
     """
     # Autograd does not allow multiple return values from
     # a differentiable function.
@@ -142,12 +145,15 @@ def _grape_schroedinger_discrete_time(gstate, initial_params):
     # functions that they evaluate on their own schedule.
     # The best solution to track mutable objects, that I can think of,
     # is to use a reporter object.
-    reporter = Reporter()
-    reporter.iteration = 0
+    reporter = GrapeResult()
 
-    result = gstate.optimizer.run((gstate, reporter), _gsd_compute_wrap,
-                                  gstate.iteration_count, initial_params,
-                                  _gsd_compute_jacobian_wrap)
+    gstate.optimizer.run((gstate, reporter), _gsd_compute_wrap,
+                         gstate.iteration_count, initial_params,
+                         _gsd_compute_jacobian_wrap)
+
+    return reporter
+
+
 
 
 def _gsd_compute(params, gstate, reporter):
@@ -156,7 +162,7 @@ def _gsd_compute(params, gstate, reporter):
     Args:
     params :: numpy.ndarray - the control parameters
     gstate :: qoc.GrapeSchroedingerDiscreteState - static objects
-    reporter :: qoc.Reporter - a reporter for mutable objects
+    reporter :: qoc.Dummy - a reporter for mutable objects
     Returns:
     total_error :: numpy.ndarray - total error of the evolution
     """
@@ -180,9 +186,9 @@ def _gsd_compute(params, gstate, reporter):
             magnus_params = anp.expand_dims(magnus_params, axis=0)
         magnus = gstate.magnus(gstate.dt, magnus_params, pulse_step, t, is_final_step)
         unitary = expm(-1j * magnus)
-        u_u_dagger = anp.matmul(unitary, conjugate_transpose(unitary))
-        identity = anp.eye(gstate.hilbert_size)
-        assert(anp.allclose(u_u_dagger, identity))
+        # u_u_dagger = anp.matmul(unitary, conjugate_transpose(unitary))
+        # identity = anp.eye(gstate.hilbert_size)
+        # assert(anp.allclose(u_u_dagger, identity))
         states = anp.matmul(unitary, states)
         
         # Compute cost.
@@ -193,7 +199,7 @@ def _gsd_compute(params, gstate, reporter):
             #ENDFOR
 
             # Report information.
-            reporter.states = states
+            reporter.last_states = states
         else:
             for i, step_cost in enumerate(gstate.step_costs):
                 error = step_cost.cost(params, states, time_step)
@@ -218,7 +224,7 @@ def _gsd_compute_jacobian_wrap(params, gstate, reporter):
     Args:
     params :: numpy.ndarray - the control parameters in optimizer format
     gstate :: qoc.GrapeSchroedingerDiscreteState - static objects
-    reporter :: qoc.Reporter - a reporter for mutable objects
+    reporter :: qoc.Dummy - a reporter for mutable objects
     Returns:
     jac :: numpy.ndarray - the jacobian of the cost function with
         respect to params in optimizer format
@@ -229,12 +235,24 @@ def _gsd_compute_jacobian_wrap(params, gstate, reporter):
 
 
     # Remove states from autograd box.
-    if isinstance(reporter.states, Box):
-        reporter.states = reporter.states._value
+    if isinstance(reporter.last_states, Box):
+        reporter.last_states = reporter.last_states._value
     # Report information.
     gstate.log_and_save(total_error, jacobian, reporter.iteration,
-                        params, reporter.states)
+                        params, reporter.last_states)
     reporter.iteration += 1
+    
+    # Update last configuration.
+    reporter.last_error = total_error
+    reporter.last_grads = jacobian
+    reporter.last_params = params
+
+    # Update minimum configuration.
+    if total_error < reporter.best_error:
+        reporter.best_error = total_error
+        reporter.best_grads = jacobian
+        reporter.best_params = params
+        reporter.best_states = reporter.last_states
 
     return _strip_params(gstate, jacobian)
 
@@ -361,7 +379,7 @@ def _strip_params(gstate, params):
     """
     # Flatten the parameters.
     params = params.flatten()
-    # Transform the parameters to C if they are complex.
+    # Transform the parameters to R2 if they are complex.
     if gstate.complex_params:
         params = complex_to_real_imag_flat(params)
 
@@ -391,13 +409,114 @@ def _slap_params(gstate, params):
 
 ### MODULE TESTS ###
 
+_BIG = 100
 
 def _test():
     """
-    Run test on the module.
+    Run tests on the module.
+    """
+    # _test_helper_functions()
+    _test_grads()
+    # _test_grape_schroedinger_discrete()
+
+
+def _test_grads():
+    """
+    Ensure derivatives are being computed properly.
+    """
+    hilbert_size = 2
+    annihilation_operator = get_annihilation_operator(hilbert_size)
+    creation_operator = get_creation_operator(hilbert_size)
+    h_system = PAULI_Z / 2
+    hamiltonian = lambda params, t: (h_system
+                                     + params[0] * annihilation_operator
+                                     + anp.conjugate(params[0]) * creation_operator)
+    initial_state_0 = anp.array([[1], [0]])
+    target_state_0 = anp.array([[0], [1]])
+    initial_states = anp.stack((initial_state_0,), axis=0)
+    target_states = anp.stack((target_state_0,), axis=0)
+    costs = [TargetInfidelity(target_states)]
+    param_count = 1
+    pulse_time = 1
+    pulse_step_count = 1
+    iteration_count = 1
+    initial_params = np.ones((pulse_step_count, param_count))
+    
+    result = grape_schroedinger_discrete(costs, hamiltonian, initial_states,
+                                         iteration_count, param_count,
+                                         pulse_step_count, pulse_time,
+                                         initial_params=initial_params)
+    
+    dt = pulse_time / pulse_step_count
+    m = np.array([[dt / 2, initial_params[0][0] * dt],
+                  [initial_params[0][0] * dt, -dt / 2]])
+    du_dm = la.expm_frechet(-1j * m, m, compute_expm=False)
+    td = np.array([[0, 1]])
+    dm_de = np.array([[0], [dt]])
+    dip_de = matmuls(td, du_dm, dm_de)[0, 0]
+    dc_de = 2 * np.abs(dip_de)
+    
+    print("analytic_grads:\n{}\nresult.last_grads:\n{}"
+          "".format(dc_de, result.last_grads))
+
+
+def _test_helper_functions():
+    """
+    Run test on the module's helper functions.
+    """
+    # Test parameter optimizer transformations.
+    gstate = Dummy()
+    gstate.complex_params = True
+    shape_range = np.arange(_BIG) + 1
+    for step_count in shape_range:
+        for param_count in shape_range:
+            gstate.params_shape = params_shape = (step_count, param_count)
+            gstate.max_param_amplitudes = np.ones(param_count)
+            params = np.random.rand(*params_shape) + 1j * np.random.rand(*params_shape)
+            stripped_params = _strip_params(gstate, params)
+            assert(stripped_params.ndim == 1)
+            assert(not stripped_params.dtype in (np.complex64, np.complex128))
+            transformed_params = _slap_params(gstate, stripped_params)
+            assert(np.array_equal(params, transformed_params))
+            assert(params.shape == transformed_params.shape)
+    #ENDFOR
+
+    gstate.complex_params = False
+    for step_count in shape_range:
+        for param_count in shape_range:
+            gstate.params_shape = params_shape = (step_count, param_count)
+            gstate.max_param_amplitudes = np.ones(param_count)
+            params = np.random.rand(*params_shape)
+            stripped_params = _strip_params(gstate, params)
+            assert(stripped_params.ndim == 1)
+            assert(not stripped_params.dtype in (np.complex64, np.complex128))
+            transformed_params = _slap_params(gstate, stripped_params)
+            assert(np.array_equal(params, transformed_params))
+            assert(params.shape == transformed_params.shape)
+    #ENDFOR
+
+    # Test parameter clipping.
+    for step_count in shape_range:
+        for param_count in shape_range:
+            params_shape = (step_count, param_count)
+            max_param_amplitudes = np.ones(param_count)
+            params = np.random.rand(*params_shape) * 2
+            _clip_params(max_param_amplitudes, params)
+            for step_params in params:
+                assert(np.less_equal(step_params, max_param_amplitudes).all())
+            params = np.random.rand(*params_shape) * -2
+            _clip_params(max_param_amplitudes, params)
+            for step_params in params:
+                assert(np.less_equal(-max_param_amplitudes, step_params).all())
+        #ENDFOR
+    #ENDFOR
+
+
+def _test_grape_schroedinger_discrete():
+    """
+    Run end-to-end test on grape_schroedinger_discrete.
     """
     # Test grape schroedinger discrete.
-
     # Evolving the state under this hamiltonian for this time should
     # perform an iSWAP. See p. 31, e.q. 109 of
     # https://arxiv.org/abs/1904.06560.
@@ -433,9 +552,8 @@ def _test():
                                          save_iteration_step=save_iteration_step,
                                          save_path=save_path,
                                          system_step_multiplier=system_step_multiplier)
-    assert(np.allclose(result.error, 0))
-    assert(np.allclose(result.states, target_states, atol=1e-03))
-    exit(0)
+    assert(np.allclose(result.last_error, 0))
+    assert(np.allclose(result.last_states, target_states, atol=1e-03))
 
     # Evolving under the zero hamiltonian should yield no change
     # in the system. Furthermore, not using parameters should
@@ -462,9 +580,9 @@ def _test():
                                          pulse_time, initial_params=initial_params,
                                          log_iteration_step=log_iteration_step,
                                          system_step_multiplier=system_step_multiplier)
-    assert(result.grads.all() == 0)
-    assert(np.allclose(initial_params, result.params))
-    assert(np.allclose(initial_states, result.states))
+    # assert(result.grads.all() == 0)
+    # assert(np.allclose(initial_params, result.params))
+    # assert(np.allclose(initial_states, result.states))
 
     # Some nontrivial gradients should appear at each time step
     # if we evolve a nontrivial hamiltonian and penalize
@@ -492,8 +610,8 @@ def _test():
                                          log_iteration_step=log_iteration_step,
                                          magnus_policy=magnus_policy,
                                          max_param_amplitudes=max_param_amplitudes)
-    assert(result.grads.all() != 0)
-    assert(not np.array_equal(result.params, initial_params))
+    # assert(result.grads.all() != 0)
+    # assert(not np.array_equal(result.params, initial_params))
 
     # If we use complex parameters on a hermitian hamiltonian,
     # the complex parameters should have no contribution to the
@@ -508,7 +626,7 @@ def _test():
                                          log_iteration_step=log_iteration_step,
                                          magnus_policy=magnus_policy,
                                          max_param_amplitudes=max_param_amplitudes)
-    assert(result.grads.imag.all() == 0)
+    # assert(result.grads.imag.all() == 0)
 
     # Parameters should be clipped if they grow too large.
     # You can log result.parameters from the test above

qoc/models/grapestate.py
@@ -213,8 +213,9 @@ class GrapeSchroedingerDiscreteState(GrapeState):
                                                      interpolation_policy,
                                                      magnus_policy)
         self.final_iteration = iteration_count - 1
-        self.final_time_step = (pulse_step_count - 1) * system_step_multiplier
-        self.dt = pulse_time / self.final_time_step
+        system_step_count = pulse_step_count * system_step_multiplier
+        self.final_time_step = system_step_count - 1
+        self.dt = pulse_time / system_step_count
         self.grape_schroedinger_policy = grape_schroedinger_policy
         self.interpolation_policy = interpolation_policy
         self.magnus_policy = magnus_policy
@@ -317,23 +318,46 @@ class GrapeResult(object):
     """
     This class encapsulates useful information about a GRAPE optimization.
     Fields:
-    error :: numpy.ndarray - the total optimization error at the final time step
+    best_error :: numpy.ndarray - the total optimization error at the final time step
+        of the iteration that achieved the lowest error
+    best_grads :: numpy.ndarray - the gradients of the cost function with respect
+        to the params at the iteration that achieved the lowest error
+    best_params :: numpy.ndarray - the parameters at the iteration that achieved
+        the lowest error
+    best_states :: numpy.ndarray - the states at the final time step of the iteration
+        that achieved the lowest error
+    iteration :: int - the current iteration
+    last_error :: numpy.ndarray - the total optimization error at the final time step
         of the last iteration
-    grads :: numpy.ndarray - the gradients of the cost function with respect
+    last_grads :: numpy.ndarray - the gradients of the cost function with respect
         to the params at the last iteration
-    params :: numpy.ndarray - the parameters at the last iteration
-    states :: numpy.ndarray - the states at the final time step of the last iteration
+    last_params :: numpy.ndarray - the parameters at the last iteration
+    last_states :: numpy.ndarray - the states at the final time step of the last iteration
     """
-    def __init__(self, error, grads,
-                 params, states):
+    def __init__(self, best_error=np.finfo(float).max, best_params=None,
+                 best_grads=None, best_states=None, iteration=0,
+                 last_error=None,
+                 last_grads=None, last_params=None, last_states=None):
         """
         See class definition for argument specifications.
         """
         super().__init__()
-        self.error = error
-        self.grads = grads
-        self.params = params
-        self.states = states
+        self.best_error = best_error
+        self.best_params = best_params
+        self.best_grads = best_grads
+        self.best_states = best_states
+        self.iteration = 0
+        self.last_error = last_error
+        self.last_grads = last_grads
+        self.last_params = last_params
+        self.last_states = last_states
+
+
+    def __str__(self):
+        return ("best_error:{},\nbest_grads:\n{}\nbest_params:\n{}\nbest_states:\n{}\n"
+                "last_error:{},\nlast_grads:\n{}\nlast_params:\n{}\nlast_states:\n{}"
+                "".format(self.best_error, self.best_grads, self.best_params, self.best_states,
+                          self.last_error, self.last_grads, self.last_params, self.last_states))
 
 
 class EvolveResult(object):