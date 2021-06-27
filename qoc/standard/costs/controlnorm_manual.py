4da3b10b494ebd27ab374b54028b80036f3c00e7
yunweilu <yunweilu2020@u.northwestern.edu>
2021-06-27 14:20:50 -0500

    yunwei's in-place gradients

---
+++ qoc/standard/costs/controlnorm_manual.py
@@ -0,0 +1,88 @@
+"""
+controlnorm.py - This module defines a cost function that penalizes
+the value of the norm of the control parameters.
+"""
+
+
+import numpy as np
+
+from qoc.models import Cost
+
+class ControlNorm_manual(Cost):
+    """
+    This cost penalizes the value of the norm of the control parameters.
+
+    Fields:
+    control_weights :: ndarray (control_eval_count x control_count)
+        - These weights, each of which should be no greater than 1,
+        represent the factor by which each control's magnitude is penalized.
+        If no weights are specified, each control's magnitude is penalized
+        equally.
+    controls_size
+    cost_multiplier
+    max_control_norms
+    name
+    requires_step_evaluation
+    """
+    name = "control_norm"
+    requires_step_evaluation = False
+
+    def __init__(self, control_count,
+                 control_eval_count,
+                 control_weights=None,
+                 cost_multiplier=1.,
+                 max_control_norms=None,):
+        """
+        See class fields for arguments not listed here.
+
+        Arguments:
+        control_count
+        control_eval_count
+        """
+        super().__init__(cost_multiplier=cost_multiplier)
+        self.control_weights = control_weights
+        self.controls_size = control_eval_count * control_count
+        self.max_control_norms = max_control_norms
+        self.control_eval_count=control_eval_count
+        self.type = "control"
+    
+    def cost(self, controls, states, system_eval_step):
+        """
+        Compute the penalty.
+
+        Arguments:
+        controls
+        states
+        system_eval_step
+
+        Returns:
+        cost
+        """
+        # Normalize the controls.
+        if self.max_control_norms is not None:
+            controls = controls / self.max_control_norms
+            
+        # Weight the controls.
+        if self.control_weights is not None:
+            controls = controls[:,] * self.control_weights
+
+        # The cost is the sum of the square of the modulus of the normalized,
+        # weighted, controls.
+        cost = np.sum(np.real(controls * np.conjugate(controls)))
+        cost_normalized = cost / self.controls_size
+        
+        return cost_normalized * self.cost_multiplier
+
+    def gradient_initialize(self, reporter):
+        return
+    def update_state(self, propagator):
+        return
+
+    def gradient(self,controls,j,k):
+        grads=(2*controls[j][k]*self.cost_multiplier)/(self.controls_size)
+        if self.max_control_norms is not None:
+            grads=grads/(self.max_control_norms)**2
+        # Weight the controls.
+        if self.control_weights is not None:
+            grads = grads * (self.control_weights[j][k]**2)
+        return grads
---
+++ qoc/standard/costs/targetstateinfidelity_manual.py
@@ -0,0 +1,97 @@
+"""
+targetstateinfidelity.py - This module defines a cost function that
+penalizes the infidelity of an evolved state and a target state.
+"""
+
+
+import numpy as np
+
+from qoc.models import Cost
+from qoc.standard.functions import conjugate_transpose, matmuls
+
+
+class TargetStateInfidelity_manual(Cost):
+    """
+    This cost penalizes the infidelity of an evolved state
+    and a target state.
+
+    Fields:
+    cost_multiplier
+    name
+    requires_step_evaluation
+    state_count
+    target_states_dagger
+    """
+    name = "target_state_infidelity"
+    requires_step_evaluation = False
+
+    def __init__(self, target_states, cost_multiplier=1., neglect_relative_phase=False):
+        """
+        See class fields for arguments not listed here.
+
+        Arguments:
+        target_states
+        """
+        super().__init__(cost_multiplier=cost_multiplier)
+        self.state_count = target_states.shape[0]
+        self.target_states_dagger = conjugate_transpose(target_states)
+        self.target_states = target_states
+        self.type = "non-control"
+        self.neglect_relative_phase = neglect_relative_phase
+    def cost(self, controls, states, system_eval_step):
+        """
+        Compute the penalty.
+
+        Arguments:
+        controls
+        states
+        system_eval_step
+
+        Returns:
+        cost
+        """
+        # The cost is the infidelity of each evolved state and its target state.
+        if self.neglect_relative_phase == False:
+            inner_products = np.matmul(self.target_states_dagger, states)[:, 0, 0]
+            self.inner_products_sum = np.sum(inner_products)
+            fidelity_normalized = np.real(
+                self.inner_products_sum * np.conjugate(self.inner_products_sum)) / self.state_count ** 2
+            infidelity = 1 - fidelity_normalized
+        else:
+            self.inner_products = np.matmul(self.target_states_dagger, states)[:, 0, 0]
+            fidelities = np.real(self.inner_products * np.conjugate(self.inner_products))
+            fidelity_normalized = np.sum(fidelities) / self.state_count
+            infidelity = 1 - fidelity_normalized
+
+        return infidelity * self.cost_multiplier
+
+    def gradient_initialize(self, reporter):
+        if self.neglect_relative_phase == False:
+            self.final_states = reporter.final_states
+            self.back_states = self.target_states * self.inner_products_sum
+        else:
+            self.final_states = reporter.final_states
+            self.back_states = np.zeros_like(self.target_states, dtype="complex_")
+            for i in range(self.state_count):
+                self.back_states[i] = self.target_states[i] * self.inner_products[i]
+
+    def update_state(self, propagator):
+        self.final_states = matmuls(propagator, self.final_states)
+        self.back_states = matmuls(propagator, self.back_states)
+
+    def gradient(self, dt, Hk):
+        grads = 0
+        if self.neglect_relative_phase == False:
+            for i in range(self.state_count):
+                grads = grads + self.cost_multiplier * (-2 * dt * np.imag(
+                    np.matmul(conjugate_transpose(self.back_states[i]), np.matmul(Hk, self.final_states[i])))) / (
+                                    self.state_count ** 2)
+        else:
+            for i in range(self.state_count):
+                grads = grads + self.cost_multiplier * (-2 * dt * np.imag(
+                    np.matmul(conjugate_transpose(self.back_states[i]), np.matmul(Hk, self.final_states[i])))) / (
+                                    self.state_count )
+        return grads
+
+
+
