8017bec76c184890d353776060f2913b991d5af2
yunweilu <yunweilu2020@u.northwestern.edu>
2021-08-08 12:33:36 -0500

    test

--- report/improvement/gradient/au500.py
+++ report/improvement/gradient/au500.py
@@ -13,11 +13,16 @@ def derivative_Expansion(HILBERT_SIZE):
     diagnol = np.arange(HILBERT_SIZE)
     up_diagnol = np.sqrt(diagnol)
     low_diagnol = np.sqrt(np.arange(1, HILBERT_SIZE + 1))
+    state = np.zeros(HILBERT_SIZE)
+    state[100]=1
     data = [low_diagnol, diagnol, up_diagnol]
     offsets = [-1, 0, 1]
     A = dia_matrix((data, offsets), shape=(HILBERT_SIZE, HILBERT_SIZE)).todense()
     B = dia_matrix(([low_diagnol, up_diagnol], [-1, 1]), shape=(HILBERT_SIZE, HILBERT_SIZE)).todense()
     c = np.block([[A, B], [np.zeros_like(A), A]])
     c=bsr_matrix(c).tocsc()
-    return expm(c)
+    state0=np.zeros_like(state)
+    state=np.block([state0,state])
+    state=expm_multiply(c,state)
+    return
 derivative_Expansion(500)
