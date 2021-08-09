57abf29390bbbec19ceb81b98b887078a47b539f
yunweilu <yunweilu2020@u.northwestern.edu>
2021-08-09 00:21:58 -0500

    test

--- report/improvement/krilov/sparse3000.py
+++ report/improvement/krilov/sparse3000.py
@@ -16,7 +16,9 @@ def krylov_sparse(HILBERT_SIZE):
     low_diagnol=np.sqrt(np.arange(1,HILBERT_SIZE+1))
     data=[low_diagnol,diagnol,up_diagnol]
     offsets=[-1,0,1]
-    A=-1j*0.1*dia_matrix((data, offsets),shape=(HILBERT_SIZE,HILBERT_SIZE))
+    A = -1j * dia_matrix((data, offsets), shape=(HILBERT_SIZE, HILBERT_SIZE)).tocsc()
+    B = -1j * dia_matrix(([low_diagnol, up_diagnol], [-1, 1]), shape=(HILBERT_SIZE, HILBERT_SIZE)).tocsc()
+    A = A +0.1* B
     state=expm_multiply(A,state)
     return state
 krylov_sparse(3000)
