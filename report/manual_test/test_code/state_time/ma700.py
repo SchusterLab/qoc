fb3862b3cba0f53cf33e1cf0685ceb81cfc115bc
yunweilu <yunweilu2020@u.northwestern.edu>
2021-10-17 11:36:41 -0500

    test

--- report/manual_test/test_code/state_time/ma700.py
+++ report/manual_test/test_code/state_time/ma700.py
@@ -120,5 +120,5 @@ def simulation(fock, dim_c, dim_trans, w_c, w_t, anharmonicity, g, evolution_tim
                                              )
     return result
 pre=2*np.pi
-dim=70
+dim=30
 simulation(2,dim,6,3.9*pre,3.5*pre,-0.225*pre,0.1*pre,1000,4000,None,5)
