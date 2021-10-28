3d15b80a104c82c570ccc4ccf3e4df0c60a793d6
yunweilu <yunweilu2020@u.northwestern.edu>
2021-10-28 12:02:11 -0500

    new

--- qoc/core/schroedingerdiscrete.py
+++ qoc/core/schroedingerdiscrete.py
@@ -609,7 +609,7 @@ def _evaluate_schroedinger_discrete(controls, pstate, reporter,pulse=None):
 
 
     else:
-        if len(pstate.initial_states) is not 1:
+        if len(pstate.initial_states) != 1:
             states = np.identity(len(pstate.initial_states))
         for system_eval_step in range(system_eval_count):
             # If applicable, save the current states.
