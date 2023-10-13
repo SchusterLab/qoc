1bf543072ae0128d64c0695850df966413a5ba01
yunweilu <yunweilu2020@u.northwestern.edu>
2023-10-13 11:20:10 -0500

    ad

--- qoc/standard/functions/expm.py
+++ qoc/standard/functions/expm.py
@@ -346,6 +346,8 @@ def expm_taylor(A, B, d=5, tol=1e-8):
         tol =1e-8
     norm_A = _exact_inf_norm(A)
     s,m=choose_ms(norm_A,d,tol)
+    s=10
+    m=10
     F=B
     for i in range(50*int(s)):
         for j in range(m):
